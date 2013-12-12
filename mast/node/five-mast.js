921112b794c3d22cf4bd1334560ee40e94e5ab42
coreindustries <corey@core-industries.com>
2013-12-12 15:45:10 -0800

    successful test of going in one direction and hitting a limit

--- mast/node/five-mast.js
+++ mast/node/five-mast.js
@@ -5,7 +5,9 @@ var five = require("johnny-five"),
 var current_step = 0; // counter
 var DIR_PIN = 2;
 var STEP_PIN = 3;
-
+var safe_to_move  = false; // flag for limit switches
+var bottom_limit  = false; // flag for bottom limit
+var top_limit     = false; // flag for top limit
 
 board = new five.Board();
 
@@ -27,23 +29,27 @@ board.on("ready", function() {
       sensor: sensor
     });
 
-    // console.log("firmata? LOW", board.firmata.LOW);
-    // console.log("firmata? HIGH", board.firmata.HIGH);
-
-    startMove('down', 1, 3000);
+    // let's kick this off
+    startMove('up', 2, 10000);
 
 
     limitBottom.on("data", function() {
-      // console.log("Bottom: ",this.value, this.raw);
       if(this.value > 1000){
         console.log(+new Date(), "BOTTOM LIMIT");
+        bottom_limit = true;
+      }else{
+        bottom_limit = false;
       }
+      // console.log("Bottom value: "+bottom_limit);
     });
     limitTop.on("data", function() {
-      // console.log("Top: ",this.value, this.raw);
       if(this.value > 1000){
         console.log(+new Date(), "TOP LIMIT");
+        top_limit = true;
+      }else{
+        top_limit = false;
       }
+      // console.log("Top value: "+top_limit);
     });
 
 
@@ -54,43 +60,67 @@ board.on("ready", function() {
 
 
 
-    /*
-    dir : "up" or "down"
-    speed : 0 - 1
-    num_steps : how far. 1000 steps = 70mm
-    */
-    function startMove(dir, speed, num_steps){
-      // console.log("START, BOARD? ",board);
-      console.log(+new Date(), "startMove. dir: "+ dir+ " speed: "+speed+" num_steps: "+num_steps);
-      speed = speed || 10;
-      steps = num_steps; // set the global variable
-      current_step = 0;
-      var direction = dir == 'up' ? 0 : 1;
-      board.digitalWrite(DIR_PIN, direction);
-      oneStep(speed, num_steps);
-    }
+/*
+dir : "up" or "down"
+speed : 0 - 1
+num_steps : how far. 1000 steps = 70mm
+*/
+function startMove(dir, speed, num_steps){
+  // console.log("START, BOARD? ",board);
+  if(!atLimit()){
+    console.log(+new Date(), "startMove. dir: "+ dir+ " speed: "+speed+" num_steps: "+num_steps);
+    speed = speed || 10;
+    steps = num_steps; // set the global variable
+    current_step = 0;
+    var direction = dir == 'up' ? 0 : 1;
+    board.digitalWrite(DIR_PIN, direction);
+    oneStep(speed, num_steps);
+  }else{
+    console.log("StartMove, but at limit. Doing nothing");
+  }
+  
+}
 
 
-    // fire one coil and then the next
-    function oneStep(speed, num_steps){
-      // console.log(+new Date(), "oneStep. speed: "+speed);
-      board.digitalWrite(STEP_PIN, board.firmata.LOW);
-      setTimeout(twoStep, speed, speed, num_steps);
-    }
 
-    // fire second coil
-    function twoStep(speed, num_steps){
-      // console.log(+new Date(), "twoStep: "+num_steps);
-      board.digitalWrite(STEP_PIN, board.firmata.HIGH);
-      // stop me if I've gone too far
-      ++current_step;
-      if(current_step == num_steps){
-        console.log(+new Date(), "exited after "+current_step+" steps");
-        current_step = 0;
-        return true;
-      }else{
-        oneStep(speed, num_steps);
-      }
-      
-    }
+// fire one coil and then the next
+function oneStep(speed, num_steps){
+  // console.log(+new Date(), "oneStep. speed: "+speed);
+  if(!atLimit()){
+    board.digitalWrite(STEP_PIN, board.firmata.LOW);
+    setTimeout(twoStep, speed, speed, num_steps);
+  }
+}
+
+
+
+// fire second coil
+function twoStep(speed, num_steps){
+  // console.log(+new Date(), "twoStep: "+num_steps);
+  board.digitalWrite(STEP_PIN, board.firmata.HIGH);
+  // stop me if I've gone too far
+  ++current_step;
+  if(current_step == num_steps){
+    console.log(+new Date(), "exited after "+current_step+" steps");
+    current_step = 0;
+    return true;
+  }else{
+    oneStep(speed, num_steps);
+  }
+}
+
+
+/**
+* Are we currently at the bottom or top limit?
+*
+*/
+function atLimit(){
+  if(bottom_limit === true || top_limit === true){
+    value = true;
+  }else{
+    value = false;
+  }
+  console.log(+new Date(), "at Limit: "+value);
+  return value;
+}
 
